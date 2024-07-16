7920c75ff92409eede5f84c2eca424f9bbdfb1f7
Tiago César <55418034+cesartiago@users.noreply.github.com>
2024-07-16 14:09:06 -0300

    Continuando 2 (+ pondo ordem)
    Lembre-se de rodar um arquivo de teste por vez, devido as características da API (até onde me lembro, eles cortam a gente se fizermos muitas requisições - por motivos de tamanho)

--- test/bibleApi.test.js
+++ test/bibleApi.test.js
@@ -3,25 +3,86 @@ const axios = require('axios');
 const BASE_URL = 'https://bible-api.com';
 
 describe('Bible API Tests', () => {
+      // Versículos Únicos
+    test('Should fetch a single verse from Exodus', async () => {
+      const response = await axios.get(`${BASE_URL}/exodus 20:13`);
+      expect(response.status).toBe(200);
+      expect(response.data).toHaveProperty('reference', 'Exodus 20:13');
+      expect(response.data.text).toMatch(/You shall not murder/);
+    });
+
+        test('Deve buscar um único versículo do Gênesis', async () => {
+        const response = await axios.get(`${BASE_URL}/genesis 1:1`);
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty('reference', 'Genesis 1:1');
+        expect(response.data.text).toMatch(/In the beginning/);
+    });
+
+    test('Deve buscar um único verso de João', async () => {
+        const response = await axios.get(`${BASE_URL}/john 3:14`);
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty('reference', 'John 3:14');
+        expect(response.data.text).toMatch(/As Moses lifted up the serpent in the wilderness, even so must the Son of Man be lifted up,/);
+    });
+
     test('Verso Único: John 3:16', async () => {
         const response = await axios.get(`${BASE_URL}/john+3:16`);
         expect(response.data.reference).toBe('John 3:16');
         expect(response.data.text).toContain('For God so loved the world, that he gave his one and only Son, that whoever believes in him should not perish, but have eternal life.');
     });
 
+    // Intervalo de Versos
     test('Intervalo de Versos: Luke 12:1-2', async () => {
         const response = await axios.get(`${BASE_URL}/luke+12:1-2`);
         expect(response.data.reference).toBe('Luke 12:1-2');
         expect(response.data.text).toContain('Meanwhile, when a multitude of many thousands had gathered together, so much so that they trampled on each other, he began to tell his disciples first of all,\n“Beware of the yeast of the Pharisees, which is hypocrisy.');
     });
+    
 
+    // Múltiplos Intervalos
     test('Múltiplos Intervalos: Romans 12:1-2,5-7,9,13:1-9', async () => {
         const response = await axios.get(`${BASE_URL}/romans+12:1-2,5-7,9,13:1-9`);
         expect(response.data.reference).toBe('Romans 12:1-2,5-7,9,13:1-9');
         expect(response.data.text).toContain('Therefore I urge you, brothers, by the mercies of God, to present your bodies a living sacrifice, holy, acceptable to God, which is your spiritual service.');
     });
 
-    test('Verso com Tradução Diferente: John 3:16 KJV', async () => {
+    test('Deve buscar vários intervalos do livro de Hebreus', async () => {
+        const response = await axios.get(`${BASE_URL}/Hebrews12:1-2,5-7,9,13:1-9&10`);
+        expect(response.status).toBe(200);
+        expect(response.data.verses.length).toBeGreaterThan(0);
+    });
+
+    test('Should fetch multiple ranges from Isaiah', async () => {
+        const response = await axios.get(`${BASE_URL}/Isaiah40:1-2,5-7`);
+        expect(response.status).toBe(200);
+        expect(response.data.verses.length).toBeGreaterThan(0);
+      });
+  
+      test('Deve buscar vários intervalos de Salmos', async () => {
+          const response = await axios.get(`${BASE_URL}/Psalms23:1-2,4-6`);
+          expect(response.status).toBe(200);
+          expect(response.data.verses.length).toBeGreaterThan(0);
+      });
+
+        // Nomes Abreviados de Livros
+    test('Deve buscar o versículo usando o nome abreviado do livro (para João)', async () => {
+        const response = await axios.get(`${BASE_URL}/jn 3:15`);
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty('reference', 'John 3:15');
+        expect(response.data.text).toMatch('that whoever believes in him should not perish, but have eternal life.');
+    });
+
+
+    test('Deve buscar o versículo usando o nome abreviado do livro (para Gênesis)', async () => {
+        const response = await axios.get(`${BASE_URL}/gen 1:1`);
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty('reference', 'Genesis 1:1');
+        expect(response.data.text).toMatch(/In the beginning/);
+    });
+
+
+    //Outros
+    test('Verso com Tradução Diferente em ingles: John 3:16 KJV', async () => {
         const response = await axios.get(`${BASE_URL}/john+3:16?translation=kjv`);
         expect(response.data.reference).toBe('John 3:16');
         expect(response.data.translation_id).toBe('kjv');
@@ -32,6 +93,24 @@ describe('Bible API Tests', () => {
         const response = await axios.get(`${BASE_URL}/?random=verse`);
         expect(response.data).toHaveProperty('text');
         expect(response.data).toHaveProperty('reference');
+    });
+
+    
+    // Números de Versículos
+    test('Deve buscar versículo com números de versículos incluídos', async () => {
+        const response = await axios.get(`${BASE_URL}/john 3:17?verse_numbers=true`);
+        expect(response.status).toBe(200);
+        expect(response.data).toHaveProperty('reference', 'John 3:17');
+        expect(response.data.text).toContain('17')
+    });
+
+    // JSONP
+    test('Deve buscar verso com retorno de chamada JSONP', async () => {
+        const response = await axios.get(`${BASE_URL}/john 3:18?callback=func`, {
+        responseType: 'text'
+        });
+        expect(response.status).toBe(200);
+        expect(response.data).toMatch(/^func\(/);
     });
 
     // test('Verso Inexistente', async () => {
@@ -63,70 +142,5 @@ describe('Bible API Tests', () => {
     //     expect(response.data.text).toContain('For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.');
     // });
 
-    test('Verso em outra tradução ', async () => {
-        const response = await axios.get(`${BASE_URL}/john+3:16?translation=almeida`);
-        expect(response.data.reference).toBe('João 3:16');
-        expect(response.data.translation_id).toBe('almeida');
-        expect(response.data.text).toContain('Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna.');
-    });
-
-     // Versículos Únicos
-    test('Deve buscar um único versículo do Gênesis', async () => {
-        const response = await axios.get(`${BASE_URL}/genesis 1:1`);
-        expect(response.status).toBe(200);
-        expect(response.data).toHaveProperty('reference', 'Genesis 1:1');
-        expect(response.data.text).toMatch(/In the beginning/);
-    });
-
-    test('Deve buscar um único verso de João', async () => {
-        const response = await axios.get(`${BASE_URL}/john 3:16`);
-        expect(response.status).toBe(200);
-        expect(response.data).toHaveProperty('reference', 'John 3:16');
-        expect(response.data.text).toMatch(/For God so loved the world/);
-    });
-
-    // Nomes Abreviados de Livros
-    test('Deve buscar o versículo usando o nome abreviado do livro (para João)', async () => {
-        const response = await axios.get(`${BASE_URL}/jn 3:15`);
-        expect(response.status).toBe(200);
-        expect(response.data).toHaveProperty('reference', 'John 3:15');
-        expect(response.data.text).toMatch('that whoever believes in him should not perish, but have eternal life.');
-    });
-
-    test('Deve buscar o versículo usando o nome abreviado do livro (para Gênesis)', async () => {
-        const response = await axios.get(`${BASE_URL}/gen 1:1`);
-        expect(response.status).toBe(200);
-        expect(response.data).toHaveProperty('reference', 'Genesis 1:1');
-        expect(response.data.text).toMatch(/In the beginning/);
-    });
-
-    // Ranges de Versículos
-    test('Deve buscar vários intervalos do livro de Hebreus', async () => {
-        const response = await axios.get(`${BASE_URL}/Hebrews12:1-2,5-7,9,13:1-9&10`);
-        expect(response.status).toBe(200);
-        expect(response.data.verses.length).toBeGreaterThan(0);
-    });
-
-    test('Deve buscar vários intervalos de Salmos', async () => {
-        const response = await axios.get(`${BASE_URL}/Psalms23:1-2,4-6`);
-        expect(response.status).toBe(200);
-        expect(response.data.verses.length).toBeGreaterThan(0);
-    });
-
-    // Números de Versículos
-    test('Deve buscar versículo com números de versículos incluídos', async () => {
-        const response = await axios.get(`${BASE_URL}/john 3:17?verse_numbers=true`);
-        expect(response.status).toBe(200);
-        expect(response.data).toHaveProperty('reference', 'John 3:17');
-        expect(response.data.text).toContain('17')
-    });
-
-    // JSONP
-    test('Deve buscar verso com retorno de chamada JSONP', async () => {
-        const response = await axios.get(`${BASE_URL}/john 3:18?callback=func`, {
-        responseType: 'text'
-        });
-        expect(response.status).toBe(200);
-        expect(response.data).toMatch(/^func\(/);
-    });
+ 
 });
---
+++ test/bibleApi2.test.js
@@ -0,0 +1,91 @@
+const axios = require('axios');
+
+const BASE_URL = 'https://bible-api.com';
+
+describe('Outros testes e Diferentes Traduções (Bible API - Testes de Integração)', () => {
+    // Erros Esperados
+ test('Deve tratar nome de livro inválido', async () => {
+    try {
+      await axios.get(`${BASE_URL}/invalidbook 1:1`);
+    } catch (error) {
+      expect(error.response.status).toBe(404);
+    }
+  });
+
+  test('Deve lidar com capítulo e versículo inválidos', async () => {
+    try {
+      await axios.get(`${BASE_URL}/john 999:999`);
+    } catch (error) {
+      expect(error.response.status).toBe(404);
+    }
+  });
+
+  // Cherokee - Novo Testamento Cherokee
+  test('Deveria buscar um versículo do Novo Testamento Cherokee', async () => {
+    const response = await axios.get(`${BASE_URL}/ᎣᏍᏛ ᎧᏃᎮᏛ ᏣᏂ ᎤᏬᏪᎳᏅᎯ+3:16?translation=cherokee`);
+    expect(response.status).toBe(200);
+    expect(response.data.translation_id).toBe('cherokee');
+    expect(response.data.reference).toBe('ᎣᏍᏛ ᎧᏃᎮᏛ ᏣᏂ ᎤᏬᏪᎳᏅᎯ 3:16');
+    // Adicione verificações específicas conforme necessário
+    expect(response.data.text).toMatch('ᎾᏍᎩᏰᏃ ᏂᎦᎥᎩ ᎤᏁᎳᏅᎯ ᎤᎨᏳᏒᎩ ᎡᎶᎯ, ᏕᎤᏲᏒᎩ ᎤᏤᎵᎦ ᎤᏪᏥ ᎾᏍᎩ ᎤᏩᏒᎯᏳ ᎤᏕᏁᎸᎯ, ᎩᎶ ᎾᏍᎩ ᏱᎪᎯᏳᎲᏍᎦ ᎤᏲᎱᎯᏍᏗᏱ ᏂᎨᏒᎾ, ᎬᏂᏛᏉᏍᎩᏂ ᎤᏩᏛᏗ.');
+  });
+
+  // Checo - Bíblia kralická
+  test('Deve buscar um versículo da Bíblia Tcheca kralická', async () => {
+    const response = await axios.get(`${BASE_URL}/Jan+3:16?translation=bkr`);
+    expect(response.status).toBe(200);
+    expect(response.data.translation_id).toBe('bkr');
+    expect(response.data.reference).toBe('Jan 3:16');
+    // Adicione verificações específicas conforme necessário
+    expect(response.data.text).toMatch('Nebo tak Bůh miloval svět, že Syna svého jednorozeného dal, aby každý, kdož věří v něho, nezahynul, ale měl život věčný.');
+  });
+
+  // Inglês - Versão Padrão Americana (1901)
+  test('Deve buscar um versículo da American Standard Version (ASV)', async () => {
+    const response = await axios.get(`${BASE_URL}/john 3:16?translation=asv`);
+    expect(response.status).toBe(200);
+    expect(response.data.translation_id).toBe('asv');
+    expect(response.data.reference).toBe('John 3:16');
+    expect(response.data.text).toMatch(/For God so loved the world/);
+  });
+
+  // Português - João Ferreira de Almeida
+  test('Deveria buscar um verso do João Ferreira de Almeida', async () => {
+    const response = await axios.get(`${BASE_URL}/joao 3:16?translation=almeida`);
+    expect(response.status).toBe(200);
+    expect(response.data.translation_id).toBe('almeida');
+    expect(response.data.reference).toBe('João 3:16');
+    expect(response.data.text).toMatch(/Porque Deus amou o mundo/);
+  });
+
+  // Latim - Vulgata Latina Clementina
+  test('Deveria buscar um verso da Vulgata Latina Clementina', async () => {
+    const response = await axios.get(`${BASE_URL}/Joannes+3:16?translation=clementine`);
+    expect(response.status).toBe(200);
+    expect(response.data.translation_id).toBe('clementine');
+    expect(response.data.reference).toBe('Joannes 3:16');
+    // Adicione verificações específicas conforme necessário
+    expect(response.data.text).toMatch('Sic enim Deus dilexit mundum, ut Filium suum unigenitum daret : ut omnis qui credit in eum, non pereat, sed habeat vitam æternam.');
+  });
+
+  
+  // Capítulos Inteiros
+  test('Should fetch entire chapter from Psalms', async () => {
+    const response = await axios.get(`${BASE_URL}/Psalms23`);
+    expect(response.status).toBe(200);
+    expect(response.data.verses.length).toBeGreaterThan(0);
+    expect(response.data.verses[0].reference).toMatch(/Psalms 23:1/);
+  });
+
+  test('Should fetch entire chapter from Matthew', async () => {
+    const response = await axios.get(`${BASE_URL}/Matthew5`);
+    expect(response.status).toBe(200);
+    expect(response.data.verses.length).toBeGreaterThan(0);
+    expect(response.data.verses[0].reference).toMatch(/Matthew 5:1/);
+  });
+
+
+});
+
+
+
